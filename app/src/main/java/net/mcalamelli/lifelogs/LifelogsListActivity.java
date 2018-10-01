523ffaeee16df478f5570b508c217c3d35651ef5
Massimiliano Calamelli <mcalamelli@gmail.com>
2018-10-01 15:35:09 +0200

    Introducing Retrofit (Gradle build)

--- app/src/main/java/net/mcalamelli/lifelogs/LifelogsListActivity.java
+++ app/src/main/java/net/mcalamelli/lifelogs/LifelogsListActivity.java
@@ -2,6 +2,13 @@ package net.mcalamelli.lifelogs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+//public interface LifelogsService {
+//    @GET("users/{user}/repos")
+//    Call<List<Repo>> listRepos(@Path("user") String user);
+//}
 
 public class LifelogsListActivity extends AppCompatActivity {
 
