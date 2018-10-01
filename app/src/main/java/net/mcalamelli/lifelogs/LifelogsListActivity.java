8847f03113d3343dae243187c12529162478bf49
Massimiliano Calamelli <mcalamelli@gmail.com>
2018-10-02 00:35:20 +0200

    Working on REST calls

--- app/src/main/java/net/mcalamelli/lifelogs/LifelogsListActivity.java
+++ app/src/main/java/net/mcalamelli/lifelogs/LifelogsListActivity.java
@@ -2,12 +2,22 @@ package net.mcalamelli.lifelogs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import retrofit2.http.GET;
-import retrofit2.http.Path;
 
+import java.util.List;
+
+import retrofit2.Call;
+//import retrofit2.http.GET;
+//import retrofit2.http.Path;
+
+//public interface Lifelog(String content, String date, String time) {
+//    this.content = content;
+//    this.date = date;
+//}
+
+//https://www.mcalamelli.net/wp-json/wp/v2/lifelogs
 //public interface LifelogsService {
 //    @GET("users/{user}/repos")
-//    Call<List<Repo>> listRepos(@Path("user") String user);
+//    Call<List<Lifelog>> listRepos(@Path("user") String user);
 //}
 
 public class LifelogsListActivity extends AppCompatActivity {
