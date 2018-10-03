407b980b01a9229ffc2de96753417c7ea09691c6
Massimiliano Calamelli <mcalamelli@gmail.com>
2018-10-03 21:02:13 +0200

    Rename data adapter

---
+++ app/src/main/java/net/mcalamelli/lifelogs/LifelogsDataAdapter.java
@@ -0,0 +1,19 @@
+package net.mcalamelli.lifelogs;
+
+public class LifelogsDataAdapter {
+    private String content;
+    private String date;
+    private String time;
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+}
